0bcb397b04e6b0f66f65d383d5cf1161b0d1999d
kysongardner <kysongardner1@gmail.com>
2020-10-24 12:56:04 -0600

    More updates on Lesson 06 and I added a link to lesson 06 on my main html page

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,13 +1,12 @@
+var t = document.getElementById('temperature').textContent
+var wS = document.getElementById('wind-speed').textContent
+
 function windChillCalculation(t, wS) {
-    if (t < 50 && wS > 3.0) {
-        let f
-        f = 35.74 + 0.6215 * t - 35.75 * wS ** 0.16 + 0.4275 * t * s ** 0.16
-        document.getElementById("wind-chill").textContent = "Wind Chill:" + f
-    }
-    else{
-        document.getElementById("wind-chill").textContent = "N/A"
+    if (t < 50 && wS > 3) {
+        var f = Math.round(35.74 + 0.6215 * t - 35.75 * wS ** 0.16 + 0.4275 * t * wS ** 0.16, 0)
+        document.querySelector("#wind-chill").innerHTML = "Wind Chill: " + f
+    } else {
+        document.querySelector("#wind-chill").innerHTML = "Wind Chill: " + "N/A"
     }
 }
-t = document.getElementById("temperature")
-wS = document.getElementById("wind-speed")
 windChillCalculation(t, wS)
