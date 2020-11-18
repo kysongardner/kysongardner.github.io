4db9a25536bdb85f02f0e9262eacc4c53a9b286e
kysongardner <kysongardner1@gmail.com>
2020-11-18 15:02:34 -0700

    Fixed some errors

--- lesson-10/weatherapi/js/weatherapi.js
+++ lesson-10/weatherapi/js/weatherapi.js
@@ -1,4 +1,4 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=da3b68c843f01e78d91a043033def649";
+const url = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=da3b68c843f01e78d91a043033def649&units=imperial";
 fetch(url)
     .then((response) => response.json())
     .then((jsObject) => {
